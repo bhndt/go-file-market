f7c100e6565d9e9a0dd9a5ad34d99e81d77ebb37
bhndt <97825684+bhndt@users.noreply.github.com>
2022-07-10 10:36:12 +0800

    Initialize feat/fix-retrieval-piece-price

---
+++ retrievalmarket/testing/test_provider_deal_environment.go
@@ -0,0 +1,60 @@
+// Package testing provides test implementations of retieval market interfaces
+package testing
+
+import (
+	"context"
+	"io"
+
+	datatransfer "github.com/filecoin-project/go-data-transfer"
+
+	rm "github.com/filecoin-project/go-fil-markets/retrievalmarket"
+	retrievalimpl "github.com/filecoin-project/go-fil-markets/retrievalmarket/impl"
+)
+
+// TestProviderDealEnvironment is a test implementation of ProviderDealEnvironment used
+// by the provider state machine.
+type TestProviderDealEnvironment struct {
+	node                    rm.RetrievalProviderNode
+	ResumeDataTransferError error
+	ReadIntoBlockstoreError error
+	TrackTransferError      error
+	UntrackTransferError    error
+	CloseDataTransferError  error
+}
+
+// NewTestProviderDealEnvironment returns a new TestProviderDealEnvironment instance
+func NewTestProviderDealEnvironment(node rm.RetrievalProviderNode) *TestProviderDealEnvironment {
+	return &TestProviderDealEnvironment{
+		node: node,
+	}
+}
+
+// Node returns a provider node instance
+func (te *TestProviderDealEnvironment) Node() rm.RetrievalProviderNode {
+	return te.node
+}
+
+func (te *TestProviderDealEnvironment) ReadIntoBlockstore(pieceData io.Reader) error {
+	return te.ReadIntoBlockstoreError
+}
+
+func (te *TestProviderDealEnvironment) TrackTransfer(deal rm.ProviderDealState) error {
+	return te.TrackTransferError
+}
+
+func (te *TestProviderDealEnvironment) UntrackTransfer(deal rm.ProviderDealState) error {
+	return te.UntrackTransferError
+}
+
+func (te *TestProviderDealEnvironment) ResumeDataTransfer(_ context.Context, _ datatransfer.ChannelID) error {
+	return te.ResumeDataTransferError
+}
+
+func (te *TestProviderDealEnvironment) CloseDataTransfer(_ context.Context, _ datatransfer.ChannelID) error {
+	return te.CloseDataTransferError
+}
+
+// TrivialTestDecider is a shortest possible DealDecider that accepts all deals
+var TrivialTestDecider retrievalimpl.DealDecider = func(_ context.Context, _ rm.ProviderDealState) (bool, string, error) {
+	return true, "", nil
+}
